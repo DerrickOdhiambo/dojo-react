60300335f68e1ad6f112141fb3f2ac976fb9c5eb
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-02-02 13:44:32 +0300

    add json server package and create a new component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <div className='content'>
-        <Home />
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+      ``
+    </Router>
   );
 }
 
