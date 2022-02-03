13e5f9441737a41882e0b9641514c1a3932576c4
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-02-03 16:49:44 +0300

    finished a react course

--- src/App.js
+++ src/App.js
@@ -2,26 +2,34 @@ import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './Components/BlogDetails';
+import NotFound from './Components/NotFound';
 
 function App() {
-  return (
-    <Router>
-      <div className='App'>
-        <Navbar />
-        <div className='content'>
-          <Switch>
-            <Route exact path='/'>
-              <Home />
-            </Route>
-            <Route path='/create'>
-              <Create />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-      ``
-    </Router>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<div className='content'>
+					<Switch>
+						<Route exact path='/'>
+							<Home />
+						</Route>
+						<Route path='/create'>
+							<Create />
+						</Route>
+						<Route path='/blogs/:id'>
+							<BlogDetails />
+						</Route>
+						<Route path='*'>
+							<NotFound />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+			``
+		</Router>
+	);
 }
 
 export default App;
